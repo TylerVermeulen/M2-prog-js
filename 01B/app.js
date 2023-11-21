3083cc59e0a424579907c05e40fc861d49ecb955
Tyler <36793@ma-web.nl>
2023-11-21 14:48:17 +0100

    added the copys

---
+++ 01B/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+        let appNaam = ("imposter");
+        let versienummer = (0.5);
+        let versiedatum = (23-11-2023)
+        let autheur = ("Tyler");
+        let copyright = ("gamers");
+        let distributeur = ("Mediacollega Amsterdam");
+        let darkmode = (true);
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
+    }
+}
+let app = new App();
+app.runApplication();
