edd95da05c584be521ed32d9387129142953e063
Tyler <36793@ma-web.nl>
2023-11-21 15:29:42 +0100

    opdracht 3

--- 01B/app.js
+++ 01B/app.js
@@ -4,21 +4,24 @@ class App
     {
         console.log("hello world!");
         //code gaat hier!
-        let appNaam = ("imposter");
-        let versienummer = (0.5);
-        let versiedatum = (23-11-2023)
-        let autheur = ("Tyler");
-        let copyright = ("gamers");
-        let distributeur = ("Mediacollega Amsterdam");
-        let darkmode = (true);
-        console.log(appNaam);
-        console.log(versienummer);
-        console.log(versiedatum);
-        console.log(autheur);
-        console.log(copyright);
-        console.log(distributeur);
-        console.log(darkmode);
+        this.appNaam = ("imposter");
+        this.versienummer = (0.5);
+        this.versiedatum = (23 + "-" + 11 + "-" + 2023)
+        this.autheur = ("Tyler");
+        this.copyright = ("gamers");
+        this.distributeur = ("Mediacollega Amsterdam");
+        this.darkmode = (true);
+        this.greeting = "Starting up";
     }
 }
 let app = new App();
 app.runApplication();
+
+console.log(app.greeting);
+console.log("appNaam: " + app.appNaam);
+console.log("versienummer: " + app.versienummer);
+console.log("versiedatum: " + app.versiedatum);
+console.log("autheur: " + app.autheur);
+console.log("copyright: " + app.copyright);
+console.log("distributeur: " + app.distributeur);
+console.log("darkmode: " + app.darkmode);
