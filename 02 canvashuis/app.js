31ab0425fd65c5b9e7cea0ff9017f4cf5e438f67
Tyler <36793@ma-web.nl>
2023-11-23 14:28:24 +0100

    opdracht 2 02

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,8 +6,12 @@ class App
         //code gaat hier!
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
-        g.fillRect(0,0,10,10);
+
         console.log(canvas)
+        g.fillStyle = "#500557";
+        g.fillRect(0,0,canvas.clientWidth,canvas.height);
+        g.fillStyle = "#00e5ff"
+        g.fillRect(0,0,10,10);
     }
 }
 let app = new App();
