a47f483244db90ef86a2d034499d537b067ebb25
Tyler <36793@ma-web.nl>
2023-11-23 14:40:13 +0100

    opdracht 2 03

---
+++ 02 lijnen/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+        let canvas = document.getElementById("canvasId");
+        let g = canvas.getContext("2d");
+
+        console.log(canvas)
+        g.beginPath()
+        g.fillStyle = "#094094"
+        g.moveTo(20,20);
+        g.lineTo(300,300);
+        g.lineTo(580,20);
+        g.closePath();
+        g.stroke();
+        g.fill()
+        g.fillStyle = "#0b1017"
+        g.stroke();
+    }
+}
+let app = new App();
+app.runApplication();
