fb26d181c263792370ed5fce12455a7a40cbf219
Tyler <36793@ma-web.nl>
2023-11-30 12:52:48 +0100

    glitch in slideshow

---
+++ 03 ifelse/app.js
@@ -0,0 +1,11 @@
+class App
+{
+    runApplication()
+    {
+        console.log("hello world!");
+        //code gaat hier!
+        let muchnews = document.getElementById("newstitle")
+    }
+}
+let app = new App();
+app.runApplication();
