3f75d250c34a2f01ecb2ea997a354248f41c691a
Tyler <36793@ma-web.nl>
2023-12-07 14:47:21 +0100

    04 2

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -1,3 +1,11 @@
+function argumentsAreHandy(shoutout){
+    console.log("do you want to give a shoutout");
+    console.log(shoutout)
+}
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("Super Shout out");
+argumentsAreHandy("hou het netjes!");
+
 function superMooieGlobalFunction(){
     console.log("ik ben global");
     console.log("dus je mag mij overal aanroepen");
