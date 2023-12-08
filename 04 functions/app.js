a5ccbc3c8d63e13d92855302fa84c380583addb7
Tyler <36793@ma-web.nl>
2023-12-08 08:21:43 +0100

    04 4

--- 04 functions/app.js
+++ 04 functions/app.js
@@ -53,6 +53,18 @@ class App
         superMooieGlobalFunction();
 
     }
+    newClassFunction(){
+        console.log("hello world in de nieuweClassFunction");
+
+        }
+    
+            anotherFunctionWithArguments(aArgument){
+        console.log("kom maar met je argument");
+        console.log("hier");
+        console.log(aArgument);
+        }
 }
 let app = new App();
 app.runApplication();
+app.newClassFunction();
+app.anotherFunctionWithArguments("mijn argument is iets...");
